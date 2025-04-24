d312786acf2091d6156ac5d46dd059df8fe444ec
nicoletta4 <130029906+nicoletta4@users.noreply.github.com>
2025-04-24 20:07:33 +0500

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,6 +1,25 @@
 def app(environ, start_response):
-    data = b"Hello, World!\n"
-    start_response(
-        "200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))]
-    )
+    method = environ["REQUEST_METHOD"]
+    path = environ["PATH_INFO"]
+
+    if method == "GET" and path == "/ping":
+        data = b"pong"
+        status = "200 OK"
+
+    elif method == "GET" and path == "/info":
+        protocol = environ["SERVER_PROTOCOL"]
+        info = f"Method: {method}\nURL: {path}\nProtocol: {protocol}"
+        data = info.encode("utf-8")
+        status = "200 OK"
+
+    else:
+        data = b"Not Found"
+        status = "404 Not Found"
+
+    headers = [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ]
+    start_response(status, headers)
     return iter([data])
+    
