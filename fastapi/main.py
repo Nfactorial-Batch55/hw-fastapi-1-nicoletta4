4f7e6b0bd863d292245972532d7a6eafbc4a79ef
nicoletta4 <130029906+nicoletta4@users.noreply.github.com>
2025-04-24 21:27:41 +0500

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -8,6 +8,11 @@ def read_root():
     return {"message": "Hello, nfactorial!"}
 
 
+@app.get("/{num}")
+def num_fact(num: int):
+    return {"factorial": math.factorial(num)}
+
+
 @app.post("/meaning-of-life")
 def meaning_of_life():
     return {"meaning": "42"}
